6d57e568d6def982dd52ed9cd8b11326c8faec7a
minamiren <minamirensh@gmail.com>
2024-09-23 04:51:27 -0500

    Starter project

---
+++ Lab1 Project/Assets/DoesNothing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoesNothing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // How does the new input system works? Looks like we don't configure it here anymore
+    }
+}
