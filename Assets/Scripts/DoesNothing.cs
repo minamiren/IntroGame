30d0ea43927a280800c796b4c0240caedbac1b97
minamiren <minamirensh@gmail.com>
2024-09-23 07:26:30 -0500

    Starter project

---
+++ Assets/Scripts/DoesNothing.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoesNothing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Not sure how input is handled with this input manager. Not in script?
+    }
+}
